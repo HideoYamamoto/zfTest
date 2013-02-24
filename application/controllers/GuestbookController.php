26b890c011dc3618fcff0ae36e3950f1512668ea
hideo <h.yamamoto.being@gmail.com>
2013-02-24 23:57:16 +0900

    #3

--- application/controllers/GuestbookController.php
+++ application/controllers/GuestbookController.php
@@ -5,12 +5,12 @@ class GuestbookController extends Zend_Controller_Action
 
     public function init()
     {
-        /* Initialize action controller here */
     }
-
+        
     public function indexAction()
     {
-        // action body
+        $guestbook = new Application_Model_GuestbookMapper();
+        $this->view->entries = $guestbook->fetchAll();
     }
 
 
